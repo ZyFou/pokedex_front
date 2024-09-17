70b6128e5d9ed3db71b9ae3c219d23f3ed6a0225
ZyFou <zyfodexe@gmail.com>
2024-09-17 14:37:39 +0200

    Feat : Module & Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PokemonListComponent} from "./pages/pokemon-list/pokemon-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PokemonListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {provideHttpClient} from "@angular/common/http";
+import { PokemonListComponent } from './pages/pokemon-list/pokemon-list.component'; // Import du provider
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(), // Ajout du provider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
