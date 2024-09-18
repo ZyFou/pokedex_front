448500779bdf7e83347d2b309b2b3c0f04141214
ZyFou <zyfodexe@gmail.com>
2024-09-18 18:27:25 +0200

    Feat : Added Background Gradient behind the pokemon

--- src/app/pages/pokemon-list/pokemon-list.component.ts
+++ src/app/pages/pokemon-list/pokemon-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import {ApiService} from "../../shared/services/api.service";
-import {Paginate} from "../../shared/interfaces/paginate";
-import {Pokemon} from "../../shared/interfaces/pokemon";
+import { ApiService } from "../../shared/services/api.service";
+import { Paginate } from "../../shared/interfaces/paginate";
+import { Pokemon } from "../../shared/interfaces/pokemon";
 
 @Component({
   selector: 'app-pokemon-list',
@@ -19,24 +19,26 @@ export class PokemonListComponent {
   }
 
   // Fonction pour charger la page suivante des pokemons
-  loadNextPokemonPage(){
+  loadNextPokemonPage() {
     // On crée le numéro de la page à charger
     let page = 1
-    if (this.pokemonList){
+    if (this.pokemonList) {
       page = this.pokemonList.current_page + 1;
     }
 
     // On charge la page si elle n'existe pas ou si on n'a pas atteint la dernière
-    if (!this.pokemonList || this.pokemonList.last_page <= page){
-      this.apiService.requestApi('/pokemon', 'GET', {page: page}).then((pokemons: Paginate<Pokemon>) => {
+    if (!this.pokemonList || this.pokemonList.last_page <= page) {
+      this.apiService.requestApi('/pokemon', 'GET', { page: page }).then((pokemons: Paginate<Pokemon>) => {
         // On ajoute les pokemons à la liste
-        if (!this.pokemonList){
+        if (!this.pokemonList) {
           this.pokemonList = pokemons;
-        }else{
+        } else {
           let datas = this.pokemonList.data.concat(pokemons.data);
-          this.pokemonList = {...pokemons, data: datas};
+          this.pokemonList = { ...pokemons, data: datas };
         }
       });
     }
   }
+
+
 }
--- src/app/shared/layouts/pokemon-card/pokemon-card.component.ts
+++ src/app/shared/layouts/pokemon-card/pokemon-card.component.ts
@@ -8,4 +8,18 @@ import { Pokemon } from "../../interfaces/pokemon";
 })
 export class PokemonCardComponent {
         @Input() pokemon!: Pokemon;
+        getBackgroundStyle(pokemon: any): string {
+                const types = pokemon.default_variety.types;
+
+                if (types.length === 1) {
+                        // Si le Pokémon n'a qu'un type, on fait un dégradé vers du blanc
+                        return `linear-gradient(120deg, ${types[0].color}, #FFFFFF)`;
+                } else if (types.length >= 2) {
+                        // Si le Pokémon a deux types, on fait un dégradé entre ces deux couleurs
+                        return `linear-gradient(120deg, ${types[1].color}, ${types[0].color})`;
+                }
+
+                // Si pas de types (dans le cas improbable), retour à une couleur par défaut
+                return 'linear-gradient(45deg, #FFFFFF, #FFFFFF)';
+        }
 }
